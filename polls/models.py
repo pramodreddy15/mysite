e4de075363b5e2c92439997efb465e440ca242c0
pramodreddy15 <b.pramod15@gmail.com>
2025-04-05 16:03:36 -0400

    database addition

--- polls/models.py
+++ polls/models.py
@@ -12,3 +12,13 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class myname(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+
+class sister_name(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.ForeignKey(myname, on_delete=models.CASCADE)
