9a67444d24de73b81598e94844c9057e4bef42ff
pramodreddy15 <b.pramod15@gmail.com>
2025-04-06 19:33:51 -0400

    database addition

---
+++ geeks/models.py
@@ -0,0 +1,12 @@
+from django.utils import timezone
+from django.db import models
+
+
+class GeeksModel(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    last_modified = models.DateTimeField(default=timezone.now)
+    img = models.ImageField(upload_to='static/img', default=None)
+
+    def __str__(self):
+        return self.title
